5dc85ea1b6747ff8d2138f23dd762bf842ea9bac
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-03-18 19:37:03 +0330

    style, refactor(Header): update styles & change Link to lowercase

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,26 +1,29 @@
 import Link from "next/link";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <nav>
-      <div>Theme Toggle Button</div>
-      <ul>
-        <li>
-          <Link href="/Contact">Contact</Link>
-        </li>
-        <li>
-          <Link href="/Technologies">Technologies</Link>
-        </li>
-        <li>
-          <Link href="/Feature">Feature</Link>
-        </li>
-        <li>
-          <Link href="/About">About</Link>
-        </li>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-      </ul>
-    </nav>
+    <header className="fixed top-0 z-50 flex h-12 w-full items-center justify-between bg-amber-400 px-12 text-lg font-semibold text-red-900">
+      <button>Theme Toggle Button</button>
+      <nav>
+        <ul className={styles.navigationList}>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/technologies">Technologies</Link>
+          </li>
+          <li>
+            <Link href="/feature">Feature</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
