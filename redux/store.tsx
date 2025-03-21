ca833480fb05940cdd6909feffed82a30c336749
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-03-22 01:50:37 +0330

    feat(store): define RootState and AppDispatch types

--- redux/store.tsx
+++ redux/store.tsx
@@ -4,4 +4,6 @@ import { GlobalSlice } from "./slices/global";
 export const store = configureStore({
   reducer: { global: GlobalSlice.reducer },
 });
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export default store;
