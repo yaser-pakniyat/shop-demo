d623ac050e322c2d29e68ec015fd862afb9d5eca
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-03-23 20:16:55 +0330

    feat(app): add Redux Provider and store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,12 +8,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/Header/Header";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <Header />
-      <Component {...pageProps} />
-    </div>
+    <Provider store={store}>
+      <div>
+        <Header />
+        <Component {...pageProps} />
+      </div>
+    </Provider>
   );
 }
