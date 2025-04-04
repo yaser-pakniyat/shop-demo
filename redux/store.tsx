78ef815573627afc116f8a2c17794975ef6248a4
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-04-04 15:57:18 +0330

    feat(store): add offCanvasReducer

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { GlobalSlice } from "./slices/global";
+import offCanvasReducer from "./slices/offCanvas";
 
 export const store = configureStore({
-  reducer: { global: GlobalSlice.reducer },
+  reducer: { global: GlobalSlice.reducer, offcanvas: offCanvasReducer },
 });
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
