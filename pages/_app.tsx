06c04749f85acfb4dd5b213ed477a172e445dc65
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-04-06 14:15:12 +0330

    feat(_app.tsx): add Footer component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import type { AppProps } from "next/app";
 import Header from "@/components/Header/Header";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
+import Footer from "@/components/footer/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <div>
         <Header />
         <Component {...pageProps} />
+        <Footer />
       </div>
     </Provider>
   );
