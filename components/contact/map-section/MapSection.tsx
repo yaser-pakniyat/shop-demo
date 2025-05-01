30fe5033581df8107a98b6c0b1a65487353ef0b5
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-05-01 23:06:42 +0330

    feat: add MapSection component

---
+++ components/contact/map-section/MapSection.tsx
@@ -0,0 +1,58 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+export default function MapSection() {
+  return (
+    <div className="col-span-2 md:col-span-1">
+<div className="h-24 ps-3">
+<h2 className="mt-8 font-bold">Find Us:</h2>
+<p className="my-3 ">Our location is available on the map.</p>
+</div>
+      <div className="grid h-[590px] w-full gap-x-2 rounded-3xl bg-sky-300 p-4 sm:grid-cols-1">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d414696.5295978688!2d50.75673979865266!3d35.70612959789037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e00491ff3dcd9%3A0xf0b3697c567024bc!2sTehran%2C%20Tehran%20Province%2C%20Iran!5e0!3m2!1sen!2s!4v1746034399011!5m2!1sen!2s"
+          width={400}
+          height={600}
+          // style={{ border: 0 }}
+          className="mx-auto mt-5 h-96 w-full rounded border-2 border-sky-400 sm:h-[394px]"
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer"
+        ></iframe>
+        <address className="space-y-4">
+          <div className="flex items-center gap-x-1">
+            <Icon
+              icon="mdi:address-marker"
+              width="28"
+              height="28"
+              className="text-red-700"
+            />
+            <p>No.123 example Street city, country</p>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Icon
+              icon="clarity:email-solid"
+              width="28"
+              height="28"
+              className="text-sky-700"
+            />
+            <a aria-label="Email" href="mailto:">
+              example@example.com
+            </a>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Icon
+              icon="f7:phone-fill"
+              width="28"
+              height="28"
+              className="text-green-700"
+            />
+            <a aria-label="Tel" href="tel:">
+              +98 (21) 12345678
+            </a>
+          </div>
+        </address>
+      </div>
+    </div>
+  );
+}
+
