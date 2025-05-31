9d7a5dcbb6570555ce52557fcda63c30088ecd8c
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-05-31 17:54:07 +0330

    feat(contact): add FormSection and MapSection to Contact Page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,5 +1,11 @@
+import FormSection from "@/components/contact/form-section/FormSection";
+import MapSection from "@/components/contact/map-section/MapSection";
+
 export default function Contact() {
-  return 
-  <main>
-  </main>;
+  return (
+    <main>
+      <FormSection />
+      <MapSection />
+    </main>
+  );
 }
