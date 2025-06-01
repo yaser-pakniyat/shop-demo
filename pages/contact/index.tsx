a4089f86d9813834f5eb050906913518c312cb07
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-06-01 21:23:08 +0330

    feat(contact): use selector to get theme from global state

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,7 +1,10 @@
 import FormSection from "@/components/contact/form-section/FormSection";
 import MapSection from "@/components/contact/map-section/MapSection";
+import { RootState } from "@/redux/store";
+import { useSelector } from "react-redux";
 
 export default function Contact() {
+  const theme = useSelector((store: RootState) => store.global.theme);
   return (
     <main>
       <FormSection />
