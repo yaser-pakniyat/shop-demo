2614f2fb9eb077dba7fe8e638efa25e44ca0374b
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-06-15 00:50:33 +0330

    feat(Typewriter.tsx): add Typewriter component using typewriter-effect library

--- components/type-writer/Typewriter.tsx
+++ components/type-writer/Typewriter.tsx
@@ -1,9 +1,10 @@
+import Typewriter from "typewriter-effect";
 import db from "@/public/db.json";
 
 export default function TypeWriter() {
   return (
     <div>
-
+      <Typewriter/>
     </div>
   );
 }
