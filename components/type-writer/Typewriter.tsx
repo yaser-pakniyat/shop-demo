cbda5e05898a07afe374b7ab188b8c081d57d498
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-06-22 00:27:51 +0330

    feat(Typewriter.tsx): complete Typewriter component

--- components/type-writer/Typewriter.tsx
+++ components/type-writer/Typewriter.tsx
@@ -4,7 +4,25 @@ import db from "@/public/db.json";
 export default function TypeWriter() {
   return (
     <div>
-      <Typewriter/>
+      <Typewriter
+        options={{
+          loop: true,
+          // autoStart: true,
+          // deleteSpeed: 80,
+          cursor: "",
+        }}
+        onInit={(typewriter) => {
+          db.icons.forEach((icon) => {
+            typewriter
+              .typeString(
+                `<strong style='background-color: grey; color: white'>${icon.techName}</strong>`,
+              )
+              .pauseFor(2000)
+              .deleteAll(100)
+              .start();
+          });
+        }}
+      />
     </div>
   );
 }
