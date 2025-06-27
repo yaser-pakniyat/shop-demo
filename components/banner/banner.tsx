e8072f2dfe79c8d3ee344a7defee8cac1a74a14c
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-06-27 18:50:36 +0330

    feat(banner.tsx): add animated loop of icons using Marquee

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -5,7 +5,17 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 export default function Banner() {
   return (
     <div>
-
+      <Marquee speed={160} pauseOnClick={true}>
+        {db.icons.map((icon) => (
+          <Icon
+            key={icon.id}
+            icon={icon.iconName}
+            width={160}
+            height={160}
+            className="mx-8"
+          ></Icon>
+        ))}
+      </Marquee>
     </div>
   );
 }
