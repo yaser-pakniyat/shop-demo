090b7827e1207541a7b8b5fd80dbb37daa6e2979
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-07-01 23:22:25 +0330

    feat(technologies): add heading, TypeWriter component, and theme-based styling

--- pages/technologies/index.tsx
+++ pages/technologies/index.tsx
@@ -1,11 +1,20 @@
 import Banner from "@/components/banner/banner";
+import TypeWriter from "@/components/type-writer/Typewriter";
 import { RootState } from "@/redux/store";
 import { useSelector } from "react-redux";
 
 export default function Technologies() {
   const theme = useSelector((store: RootState) => store.global.theme);
   return (
-    <div>
+    <div
+      className={`wrapper ${theme === "dark" ? "darkWrapper" : "lightWrapper"}`}
+    >
+      <div className="mb-16 flex justify-center sm:text-3xl sm:font-bold">
+        <h1 className="mr-2 flex items-center">
+          This website has been developed with
+        </h1>
+        <TypeWriter />
+      </div>
       <Banner />
     </div>
   );
