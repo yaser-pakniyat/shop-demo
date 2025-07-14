01e8c61997a8b82a2e7c726331354b7d94b54b1a
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-07-15 01:04:59 +0330

    chore(MobileCard.tsx): define cardWrapper using Tailwind classes

--- components/mobile-card/MobileCard.tsx
+++ components/mobile-card/MobileCard.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { Mobile } from "@/types/mobileTypes";
 
+const cardWrapper =
+  "group col-span-12 flex max-w-96 cursor-pointer flex-col items-center overflow-hidden border border-gray-200 sm:col-span-6 md:col-span-6 lg:col-span-4";
+
 export default function MobileCard() {
   return <div></div>;
 }
