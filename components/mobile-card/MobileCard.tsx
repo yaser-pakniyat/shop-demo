d0dd5dfbc78eef3127f3537cc72e3ee87e1ea000
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-07-16 00:43:57 +0330

    style(MobileCard.tsx): apply cardWrapper styles to root div

--- components/mobile-card/MobileCard.tsx
+++ components/mobile-card/MobileCard.tsx
@@ -6,5 +6,5 @@ const cardWrapper =
   "group col-span-12 flex max-w-96 cursor-pointer flex-col items-center overflow-hidden border border-gray-200 sm:col-span-6 md:col-span-6 lg:col-span-4";
 
 export default function MobileCard() {
-  return <div></div>;
+  return <div className={cardWrapper}></div>;
 }
