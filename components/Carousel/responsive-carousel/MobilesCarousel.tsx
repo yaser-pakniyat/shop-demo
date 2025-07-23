f4fb4479f030868e693e103383f6cae2350e1143
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-07-23 12:49:56 +0330

    chore(MobilesCarousel.tsx): import useDispatch from react-redux

--- components/Carousel/responsive-carousel/MobilesCarousel.tsx
+++ components/Carousel/responsive-carousel/MobilesCarousel.tsx
@@ -1,5 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 export default function MobilesCarousel() {
   <div></div>;
