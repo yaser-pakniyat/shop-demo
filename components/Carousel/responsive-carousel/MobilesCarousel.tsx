6c8dbb2f4ab4500974e5642f5cc0094ca6d56a6b
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-07-25 15:02:59 +0330

    style(MobilesCarousel.tsx): import Swiper base style

--- components/Carousel/responsive-carousel/MobilesCarousel.tsx
+++ components/Carousel/responsive-carousel/MobilesCarousel.tsx
@@ -1,6 +1,7 @@
-import { Swiper, SwiperSlide } from "swiper/react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 export default function MobilesCarousel() {
   <div></div>;
