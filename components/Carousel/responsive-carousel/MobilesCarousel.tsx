46717dff7000f3c25d94488277c1dd34ce24dfc8
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-07-26 13:13:41 +0330

    chore(MobilesCarousel.tsx): import RootState for Redux typing

--- components/Carousel/responsive-carousel/MobilesCarousel.tsx
+++ components/Carousel/responsive-carousel/MobilesCarousel.tsx
@@ -1,3 +1,4 @@
+import { RootState } from "@/redux/store";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
