4d660496fce91fb3ed66dfd546b085e97e2c9ded
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-08-07 16:20:13 +0330

    chore(MobilesFetch): import useDispatch from react-redux

--- components/mobiles-fetch/mobilsFetch.tsx
+++ components/mobiles-fetch/mobilsFetch.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import MobileCard from "../mobile-card/MobileCard";
+import { useDispatch } from "react-redux";
+
 export default function MobilesFetch() {
   return <div></div>;
 }
