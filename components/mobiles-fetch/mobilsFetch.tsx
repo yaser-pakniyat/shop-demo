bdfd9a5714e9dd3a100675ace6dbc6747fc059ac
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-08-11 23:24:48 +0330

    chore(MobilesFetch): import fetchMobiles thunk from mobilesSlice

--- components/mobiles-fetch/mobilsFetch.tsx
+++ components/mobiles-fetch/mobilsFetch.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import MobileCard from "../mobile-card/MobileCard";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store";
+import { fetchMobiles } from "@/redux/slices/mobilesSlice";
 
 export default function MobilesFetch() {
   return <div></div>;
