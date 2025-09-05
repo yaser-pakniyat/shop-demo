bd1caf4d1d728823ab8a7a82698c9ae0d06a1323
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-09-05 18:39:59 +0330

    chore(Shop): import mobilesPoster SVG image

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import mobilesPoster from "../../public/posters/mobiles.svg";
 
 export default function Shop() {
   return (
     <div>
-
+      
     </div>
   );
 }
