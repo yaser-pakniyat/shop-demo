e93b5c441a8f7c6e6cd575b631d76586f0b6bbfe
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-09-08 13:29:50 +0330

    style(Shop): add styles to Image component

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -6,7 +6,13 @@ export default function Shop() {
   return (
     <div className="wrapper flex flex-col items-center gap-y-4 sm:flex-row sm:justify-evenly">
       <Link href="/shop/mobilestore">
-        <Image width={1280} height={720} alt="mobiles" src={mobilesPoster} />
+        <Image
+          width={1280}
+          height={720}
+          alt="mobiles"
+          src={mobilesPoster}
+          className="w-96 rounded-2xl bg-sky-200"
+        />
       </Link>
     </div>
   );
