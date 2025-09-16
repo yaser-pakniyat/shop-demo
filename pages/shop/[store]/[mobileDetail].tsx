832b3c1c4fa82e2869e06320cfd8b204a9673b01
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-09-17 00:16:14 +0330

    chore(MobileDetail): import useRouter

--- pages/shop/[store]/[mobileDetail].tsx
+++ pages/shop/[store]/[mobileDetail].tsx
@@ -1,5 +1,7 @@
 import { fetchMobiles } from "@/redux/slices/mobilesSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/router";
+
 export default function MobileDetail() {
   return (
     <div>
