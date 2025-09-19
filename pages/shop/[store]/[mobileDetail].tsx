d82e47e6bb107c5a7d97ca67c2f218ebe82b56c2
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-09-19 21:13:08 +0330

    chore(MobileDetail): import AppDispatch type from store

--- pages/shop/[store]/[mobileDetail].tsx
+++ pages/shop/[store]/[mobileDetail].tsx
@@ -2,7 +2,7 @@ import { fetchMobiles } from "@/redux/slices/mobilesSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-
+import { AppDispatch } from "@/redux/store";
 export default function MobileDetail() {
   return (
     <div>
