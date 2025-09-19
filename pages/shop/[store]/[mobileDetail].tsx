e8404dd99bf69cd363f8c4714b1981931a350b22
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-09-20 00:11:39 +0330

    chore(MobileDetail): import RootState type from store

--- pages/shop/[store]/[mobileDetail].tsx
+++ pages/shop/[store]/[mobileDetail].tsx
@@ -2,7 +2,8 @@ import { fetchMobiles } from "@/redux/slices/mobilesSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { AppDispatch } from "@/redux/store";
+import { AppDispatch, RootState } from "@/redux/store";
+
 export default function MobileDetail() {
   return (
     <div>
