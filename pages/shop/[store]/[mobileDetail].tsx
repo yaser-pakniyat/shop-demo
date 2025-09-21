e9d453694d4d20f7ca95910515b7e7e8cfdae9ae
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-09-21 21:17:08 +0330

    chore(MobileDetail): import Image component from next/image

--- pages/shop/[store]/[mobileDetail].tsx
+++ pages/shop/[store]/[mobileDetail].tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { AppDispatch, RootState } from "@/redux/store";
+import Image from "next/image";
 
 export default function MobileDetail() {
   return (
