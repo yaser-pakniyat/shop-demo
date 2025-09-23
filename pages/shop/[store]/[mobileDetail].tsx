9f5eb28101cdce2712bb88d962b6cda034ffb7ca
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-09-23 11:28:37 +0330

    chore(MobileDetail): add useDispatch hook with AppDispatch type

--- pages/shop/[store]/[mobileDetail].tsx
+++ pages/shop/[store]/[mobileDetail].tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { RiseLoader } from "react-spinners";
 
 export default function MobileDetail() {
+    const dispatch = useDispatch<AppDispatch>();
   return (
     <div>
 
