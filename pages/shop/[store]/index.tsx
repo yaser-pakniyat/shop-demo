2118a61c1b87afe79fc93bcce132f02e2ce33c4d
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-10-13 22:33:32 +0330

    chore(Store): import TabletStore component

--- pages/shop/[store]/index.tsx
+++ pages/shop/[store]/index.tsx
@@ -1,6 +1,6 @@
 import MobileStore from "@/components/store/mobileStore";
+import TabletStore from "@/components/store/tabletStore";
 import { useRouter } from "next/router";
-
 export default function Store() {
   return (
     <div></div>
