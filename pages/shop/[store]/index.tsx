da821ca9cb7768389c43a6c65c26096d49b28a25
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-10-14 17:11:19 +0330

    chore(Store): initialize router and extract store param from query

--- pages/shop/[store]/index.tsx
+++ pages/shop/[store]/index.tsx
@@ -2,6 +2,8 @@ import MobileStore from "@/components/store/mobileStore";
 import TabletStore from "@/components/store/tabletStore";
 import { useRouter } from "next/router";
 export default function Store() {
+  const router = useRouter();
+  const store = router.query.store || [];
   return (
     <div></div>
   )
