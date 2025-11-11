ff20788e187b9dc1460a78a117ba8932f8e08f4b
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-11-11 10:29:23 +0330

    chore(app): remove commented-out code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,3 @@
-// import "@/styles/globals.css";
-// import type { AppProps } from "next/app";
-
-// export default function App({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />;
-// }
-
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/Header/Header";
