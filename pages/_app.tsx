0ea8579c83e3b6f26f69015b7612a1dd82ef0340
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-11-12 13:02:38 +0330

    chore(app): remove Header since it's handled by AppLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Header from "@/components/Header/Header";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
 import Footer from "@/components/footer/Footer";
@@ -10,7 +9,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <div>
-        <Header />
         <AppLayout>
           <Component {...pageProps} />
         </AppLayout>
