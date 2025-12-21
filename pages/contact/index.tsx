22944387fbb66627b61c2f7a888dfda261f1541d
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-12-21 18:08:05 +0330

    refactor(contact): remove theme-based class condition since it's handled by AppLayout

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -6,9 +6,7 @@ import { useSelector } from "react-redux";
 export default function Contact() {
   const theme = useSelector((store: RootState) => store.global.theme);
   return (
-    <main
-      className={`grid grid-cols-2 gap-x-4 gap-y-4 px-8 py-12 transition duration-1000 ease-out ${theme === "dark" ? "darkWrapper" : "lightWrapper"}`}
-    >
+    <main className="grid grid-cols-2 gap-x-4 gap-y-4 px-8 py-12 transition duration-1000 ease-out">
       <FormSection />
       <MapSection />
     </main>
