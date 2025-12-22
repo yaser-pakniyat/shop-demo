33c076a4b76f2f6d977e0f3e8a087e6958a18685
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-12-22 12:46:59 +0330

    style(contact): change transition timing function from ease-out to ease-linear

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 export default function Contact() {
   const theme = useSelector((store: RootState) => store.global.theme);
   return (
-    <main className="grid grid-cols-2 gap-x-4 gap-y-4 px-8 py-12 transition duration-1000 ease-out">
+    <main className="grid grid-cols-2 gap-x-4 gap-y-4 px-8 py-12 transition duration-1000 ease-linear">
       <FormSection />
       <MapSection />
     </main>
