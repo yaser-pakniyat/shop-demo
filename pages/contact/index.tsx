f6911afe0d9727f74f9fdeb0a8eca78bdda37640
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2025-12-24 11:15:48 +0330

    refactor(contact): remove local theme selector since handled by AppLayout

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,10 +1,7 @@
 import FormSection from "@/components/contact/form-section/FormSection";
 import MapSection from "@/components/contact/map-section/MapSection";
-import { RootState } from "@/redux/store";
-import { useSelector } from "react-redux";
 
 export default function Contact() {
-  const theme = useSelector((store: RootState) => store.global.theme);
   return (
     <main className="contactStyles">
       <FormSection />
