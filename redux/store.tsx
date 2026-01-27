c71757a0007f4479ceb14410387f27462f4a9cdd
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-01-27 23:02:15 +0330

    feat(store): add products slice to Redux store

--- redux/store.tsx
+++ redux/store.tsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { GlobalSlice } from "./slices/global";
 import offCanvasReducer from "./slices/offCanvas";
 import mobileReducer from "@/redux/slices/mobilesSlice";
+import { productsSlice } from "./slices/productsSlice";
 
 export const store = configureStore({
   reducer: {
     global: GlobalSlice.reducer,
     offcanvas: offCanvasReducer,
     mobiles: mobileReducer,
+    products: productsSlice.reducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
