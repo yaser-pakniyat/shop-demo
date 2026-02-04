0f55836ae3ac33961e91d6d8e23e43b0ccc368a7
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-02-04 15:48:31 +0330

    chore(TabletCard): import Link component for future use

--- components/tablet-card/TabletCard.tsx
+++ components/tablet-card/TabletCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Tablet } from "@/types/tabletTypes";
 
 export default function TabletCard({ id, tabletName, address, price }: Tablet) {
