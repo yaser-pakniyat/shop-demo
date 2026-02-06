12ac24feea7093a4e3499e5b576db6390c12b3ac
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-02-06 18:22:34 +0330

    feat(TabletCard): render image and name inside Link to tablet detail

--- components/tablet-card/TabletCard.tsx
+++ components/tablet-card/TabletCard.tsx
@@ -3,5 +3,18 @@ import Link from "next/link";
 import { Tablet } from "@/types/tabletTypes";
 
 export default function TabletCard({ id, tabletName, address, price }: Tablet) {
-  return <div className="cardWrapper"></div>;
+  return (
+    <div className="cardWrapper">
+      <Link href={`/shop/tabletstore/${tabletName}`}>
+        <Image
+          alt={tabletName}
+          src={address}
+          width={1280}
+          height={720}
+          priority
+        />
+        <h2 className="flex justify-center">{tabletName}</h2>
+      </Link>
+    </div>
+  );
 }
