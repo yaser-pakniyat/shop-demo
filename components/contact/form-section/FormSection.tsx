6f4391c5e9cb56c33b92387ae99e2cddf44038d7
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-02-16 14:36:51 +0330

    style(FormSection): set input text color to black

--- components/contact/form-section/FormSection.tsx
+++ components/contact/form-section/FormSection.tsx
@@ -29,7 +29,7 @@ export default function FormSection() {
         {db.inputs.map((input: Input) => (
           <input
             key={input.id}
-            className="mt-5 w-full rounded border-2 border-sky-400 bg-sky-100 p-2 placeholder-sky-400 outline-none"
+            className="mt-5 w-full rounded border-2 border-sky-400 bg-sky-100 p-2 text-black placeholder-sky-400 outline-none"
             type={input.type}
             name={input.name}
             placeholder={input.placeholder}
