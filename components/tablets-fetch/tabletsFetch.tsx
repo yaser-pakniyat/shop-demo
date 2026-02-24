f9b6939731c59bbb7d3350b8b992e29eaf03b439
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-02-24 12:47:12 +0330

    chore(TabletsFetch): import useSelector from react-redux

--- components/tablets-fetch/tabletsFetch.tsx
+++ components/tablets-fetch/tabletsFetch.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function TabletsFetch() {
   return <div></div>;
