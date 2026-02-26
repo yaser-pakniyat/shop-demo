f6b3af25b15c7fc3fa1d1c20e8cd30ddbc12eab0
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-02-26 13:31:13 +0330

    chore(TabletsFetch): import RootState for Redux typing

--- components/tablets-fetch/tabletsFetch.tsx
+++ components/tablets-fetch/tabletsFetch.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import TabletCard from "../tablet-card/TabletCard";
 import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 
 export default function TabletsFetch() {
   return <div></div>;
