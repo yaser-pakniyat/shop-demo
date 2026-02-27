ef97beeb1c45f7d82e134d6391cf87847343a658
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-02-27 16:46:09 +0330

    chore(TabletsFetch): import AppDispatch type from store

--- components/tablets-fetch/tabletsFetch.tsx
+++ components/tablets-fetch/tabletsFetch.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import TabletCard from "../tablet-card/TabletCard";
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
+import { AppDispatch, RootState } from "@/redux/store";
 
 export default function TabletsFetch() {
   return <div></div>;
