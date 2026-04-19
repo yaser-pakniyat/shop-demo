1dfb4f6a3b95ecc0d4a56d2257e4b4aa111273b3
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-04-19 16:10:50 +0330

    chore(TabletsFetch): import fetchTablets thunk from tabletsSlice

--- components/tablets-fetch/tabletsFetch.tsx
+++ components/tablets-fetch/tabletsFetch.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import TabletCard from "../tablet-card/TabletCard";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store";
+import { fetchTablets } from "@/redux/slices/tabletsSlice";
 
 export default function TabletsFetch() {
   return <div></div>;
