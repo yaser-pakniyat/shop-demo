73b744ad7e0e7d8029916ba0e9a1f7d8a3ad5d29
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-04-20 14:43:36 +0330

    chore(TabletsFetch): import RiseLoader from react-spinners

--- components/tablets-fetch/tabletsFetch.tsx
+++ components/tablets-fetch/tabletsFetch.tsx
@@ -3,6 +3,7 @@ import TabletCard from "../tablet-card/TabletCard";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store";
 import { fetchTablets } from "@/redux/slices/tabletsSlice";
+import { RiseLoader } from "react-spinners";
 
 export default function TabletsFetch() {
   return <div></div>;
