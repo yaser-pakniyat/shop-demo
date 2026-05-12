d286dbd4df3a82fc862621946911b908e0ac5fec
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-05-12 11:29:46 +0330

    chore(TabletsCarousel): import useDispatch from react-redux

--- components/Carousel/responsive-carousel/TabletsCarousel.tsx
+++ components/Carousel/responsive-carousel/TabletsCarousel.tsx
@@ -1,6 +1,7 @@
 import TabletCard from "@/components/tablet-card/TabletCard";
 import { fetchProducts } from "@/redux/slices/productsSlice";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 export default function TabletsCarousel() {
 
