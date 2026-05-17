ab7ec9301dd7c80817ba7e2b6facbf7d06440b01
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-05-17 23:04:32 +0330

    chore(TabletsCarousel): import RootState type

--- components/Carousel/responsive-carousel/TabletsCarousel.tsx
+++ components/Carousel/responsive-carousel/TabletsCarousel.tsx
@@ -2,7 +2,7 @@ import TabletCard from "@/components/tablet-card/TabletCard";
 import { fetchProducts } from "@/redux/slices/productsSlice";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch } from "@/redux/store";
+import { AppDispatch, RootState } from "@/redux/store";
 
 export default function TabletsCarousel() {
 
