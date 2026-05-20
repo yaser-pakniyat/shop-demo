288c4ac24b6ae8a2613a58038beb765818039089
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-05-20 17:11:47 +0330

    chore(TabletsCarousel): import Swiper base style

--- components/Carousel/responsive-carousel/TabletsCarousel.tsx
+++ components/Carousel/responsive-carousel/TabletsCarousel.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 export default function TabletsCarousel() {
 
