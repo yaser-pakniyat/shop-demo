370b21964ad1ce7114ce5673a91d74dc44e55388
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-05-26 14:41:11 +0330

    chore(TabletStore): import TabletsFetch component

--- components/store/tabletStore.tsx
+++ components/store/tabletStore.tsx
@@ -1,4 +1,5 @@
 import TabletsCarousel from "../Carousel/responsive-carousel/TabletsCarousel";
+import TabletsFetch from "@/components/tablets-fetch/tabletsFetch";
 
 export default function TabletStore() {
   return (
