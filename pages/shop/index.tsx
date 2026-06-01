464605febff1952017f84a9ccaeeb37abeea6d98
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-06-01 11:57:04 +0330

    refactor(shop): replace static poster rendering with data-driven approach

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,29 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
-import mobilesPoster from "../../public/posters/mobiles.svg";
-import tabletsPoster from "../../public/posters/tablets.svg";
+import { posters } from "@/public/db.json";
 
 export default function Shop() {
   return (
     <div className="wrapper mt-24 flex flex-col items-center sm:flex-row sm:justify-evenly">
-      <Link href="/shop/mobilestore">
-        <Image
-          width={1280}
-          height={720}
-          alt="mobiles"
-          src={mobilesPoster}
-          className="w-96 rounded-2xl bg-sky-200"
-        />
-      </Link>
-      <Link href="/shop/tabletstore">
-        <Image
-          width={1280}
-          height={720}
-          alt="tablets"
-          src={tabletsPoster}
-          className="w-96 rounded-2xl bg-sky-200"
-        />
-      </Link>
+      {posters.map((poster) => (
+        <Link key={poster.id} href={poster.href}>
+          <Image
+            width={1280}
+            height={720}
+            alt={poster.alt}
+            src={poster.src}
+            className="w-96 rounded-2xl bg-sky-200"
+          />
+        </Link>
+      ))}
     </div>
   );
 }
