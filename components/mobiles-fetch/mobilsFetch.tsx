7f29defb3b838f2f7d069ffa149e26970d1382d5
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-06-02 15:14:40 +0330

    refactor(MobilesFetch): migrate mobile data access to productsSlice

--- components/mobiles-fetch/mobilsFetch.tsx
+++ components/mobiles-fetch/mobilsFetch.tsx
@@ -2,29 +2,29 @@ import { useEffect } from "react";
 import MobileCard from "../mobile-card/MobileCard";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store";
-import { fetchMobiles } from "@/redux/slices/mobilesSlice";
+import { fetchProducts } from "@/redux/slices/productsSlice";
 import { RiseLoader } from "react-spinners";
 
 export default function MobilesFetch() {
-  const mobiles = useSelector((store: RootState) => store.mobiles.mobiles);
-  const pending = useSelector((store: RootState) => store.mobiles.pending);
+  const products = useSelector((store: RootState) => store.products.products);
+  const pending = useSelector((store: RootState) => store.products.pending);
   const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
-    dispatch(fetchMobiles());
+    dispatch(fetchProducts());
   }, [dispatch]);
-  if (mobiles.length === 0) {
+  if (products.length === 0) {
     return (
       <div className="flex h-screen items-center justify-center">
         <RiseLoader color="gray" />
       </div>
     );
   }
-  if (!pending && mobiles.length === 0) {
+  if (!pending && products.length === 0) {
     return <div>No Mobile exists</div>;
   }
   return (
     <div className="mx-14 grid grid-cols-12 gap-y-2 sm:gap-x-2">
-      {mobiles.map((mobile) => (
+      {products.slice(0, 12).map((mobile) => (
         <MobileCard key={mobile.id} {...mobile} />
       ))}
     </div>
