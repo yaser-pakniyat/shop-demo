f3de38f1fe05f889f62de99374302d206092d8f3
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-06-07 17:36:41 +0330

    refactor(store): remove mobile reducer

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { GlobalSlice } from "./slices/global";
 import offCanvasReducer from "./slices/offCanvas";
-import mobileReducer from "@/redux/slices/mobilesSlice";
 import { productsSlice } from "./slices/productsSlice";
 
 export const store = configureStore({
   reducer: {
     global: GlobalSlice.reducer,
     offcanvas: offCanvasReducer,
-    mobiles: mobileReducer,
     products: productsSlice.reducer,
   },
 });
