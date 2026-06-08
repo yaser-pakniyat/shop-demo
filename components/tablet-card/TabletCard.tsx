5bef4ecfce50c5685ae141e826e2b8db5800ae5c
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-06-08 15:30:24 +0330

    refactor(TabletCard): replace tablet data with product data

--- components/tablet-card/TabletCard.tsx
+++ components/tablet-card/TabletCard.tsx
@@ -1,25 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Tablet } from "@/types/tabletTypes";
+import { Product } from "@/types/productTypes";
 
-export default function TabletCard({ id, tabletName, address, price }: Tablet) {
+export default function TabletCard({
+  id,
+  productName,
+  address,
+  price,
+}: Product) {
   return (
     <div className="cardWrapper group">
-      <Link href={`/shop/tabletstore/${tabletName}`}>
+      <Link href={`/shop/tabletstore/${productName}`}>
         <Image
-          alt={tabletName}
+          alt={productName}
           src={address}
           width={1280}
           height={720}
           priority
           className="transition-transform duration-[2000ms] ease-linear group-hover:scale-110"
         />
-        <h2 className="flex justify-center">{tabletName}</h2>
+        <h2 className="flex justify-center">{productName}</h2>
       </Link>
 
       <Link
         className="my-3 cursor-pointer rounded border bg-gray-300 px-3 py-1 hover:scale-90 hover:bg-green-500 hover:font-bold hover:text-white"
-        href={`/shop/tabletstore/${tabletName}`}
+        href={`/shop/tabletstore/${productName}`}
       >
         more info
       </Link>
