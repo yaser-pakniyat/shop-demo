738ee651e3fad0d7ec2a1668d9a0bb0b2dd410c8
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-06-09 11:57:56 +0330

    refactor(MobileCard): replace mobile data with product data

--- components/mobile-card/MobileCard.tsx
+++ components/mobile-card/MobileCard.tsx
@@ -1,24 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Mobile } from "@/types/mobileTypes";
+import { Product } from "@/types/productTypes";
 
-export default function MobileCard({ id, mobileName, address, price }: Mobile) {
+export default function MobileCard({
+  id,
+  productName,
+  address,
+  price,
+}: Product) {
   return (
     <div className="cardWrapper group">
-      <Link href={`/shop/mobilestore/${mobileName}`}>
+      <Link href={`/shop/mobilestore/${productName}`}>
         <Image
-          alt={mobileName}
+          alt={productName}
           src={address}
           width={1280}
           height={720}
           priority
           className="transition-transform duration-[2000ms] ease-linear group-hover:scale-110"
         />
-        <h2 className="flex justify-center">{mobileName}</h2>
+        <h2 className="flex justify-center">{productName}</h2>
       </Link>
       <Link
         className="my-3 cursor-pointer rounded border bg-gray-300 px-3 py-1 hover:scale-90 hover:bg-green-500 hover:font-bold hover:text-white"
-        href={`/shop/mobilestore/${mobileName}`}
+        href={`/shop/mobilestore/${productName}`}
       >
         more info
       </Link>
