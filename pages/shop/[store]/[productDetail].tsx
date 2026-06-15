425cec156a874e2ed8255433e827293aef1827bc
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-06-15 14:47:27 +0330

    chore(ProductDetail): import useEffect

--- pages/shop/[store]/[productDetail].tsx
+++ pages/shop/[store]/[productDetail].tsx
@@ -1,6 +1,7 @@
 import { fetchProducts } from "@/redux/slices/productsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function ProductDetail() {
   return (
