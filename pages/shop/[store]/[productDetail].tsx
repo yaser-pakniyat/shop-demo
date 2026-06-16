8ee16246f1ecee1799fc36c9f7e33ceb01bc4004
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-06-16 05:35:45 +0330

    chore(ProductDetail): import AppDispatch type from store

--- pages/shop/[store]/[productDetail].tsx
+++ pages/shop/[store]/[productDetail].tsx
@@ -2,6 +2,7 @@ import { fetchProducts } from "@/redux/slices/productsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { AppDispatch } from "@/redux/store";
 
 export default function ProductDetail() {
   return (
