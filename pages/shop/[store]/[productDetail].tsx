dff82eae694d9237310d805ff6d31749ebf7eb52
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-06-20 14:02:42 +0330

    chore(ProductDetail): add useDispatch hook with AppDispatch type

--- pages/shop/[store]/[productDetail].tsx
+++ pages/shop/[store]/[productDetail].tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { RiseLoader } from "react-spinners";
 
 export default function ProductDetail() {
+  const dispatch = useDispatch<AppDispatch>();
   return (
     <div>
 
