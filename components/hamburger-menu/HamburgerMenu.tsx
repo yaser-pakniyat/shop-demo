b578dcf2a6ffb41214d2bdd3310fa3a338d3f542
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-07-04 15:02:17 +0330

    feat(HamburgerMenu): add backdrop and close OffCanvas menu on backdrop click

--- components/hamburger-menu/HamburgerMenu.tsx
+++ components/hamburger-menu/HamburgerMenu.tsx
@@ -19,38 +19,49 @@ export default function HamburgerMenu() {
     dispatch(closeOffcanvas());
   };
   return (
-    <aside
-      className={`fixed top-0 right-0 z-[60] h-screen w-1/2 transition-all duration-1000 ease-linear ${isOpen ? "translate-x-0" : "translate-x-full"} ${theme === "dark" ? "bg-stone-700" : "bg-green-800"} md:translate-x-full`}
-    >
-      {/* close button start */}
-      <div className="pt-3 pl-3">
-        <Icon
-          icon="line-md:close"
-          width="26"
-          height="26"
-          className="cursor-pointer hover:scale-105"
+    <>
+      {isOpen && (
+        <div
+          className="fixed inset-0 z-50 bg-black/30"
           onClick={CloseOffCanvasHandler}
-        />
-      </div>
-      {/* close button end */}
+        ></div>
+      )}
+      <aside
+        className={`fixed top-0 right-0 z-[60] h-screen w-1/2 transition-all duration-1000 ease-linear ${isOpen ? "translate-x-0" : "translate-x-full"} ${theme === "dark" ? "bg-stone-700" : "bg-green-800"} md:translate-x-full`}
+      >
+        {/* close button start */}
+        <div className="pt-3 pl-3">
+          <Icon
+            icon="line-md:close"
+            width="26"
+            height="26"
+            className="cursor-pointer hover:scale-105"
+            onClick={CloseOffCanvasHandler}
+          />
+        </div>
+        {/* close button end */}
 
-      {/* map on menu start */}
-      <nav>
-        <ul className="">
-          {db.menu.map((menu: Menu) => (
-            <li key={menu.id} className="h-12 w-full text-xl hover:bg-blue-950">
-              <Link
-                className="flex h-full w-full items-center justify-center hover:scale-105"
-                href={menu.href}
-                onClick={CloseOffCanvasHandler}
+        {/* map on menu start */}
+        <nav>
+          <ul className="">
+            {db.menu.map((menu: Menu) => (
+              <li
+                key={menu.id}
+                className="h-12 w-full text-xl hover:bg-blue-950"
               >
-                {menu.linkName}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      {/* map on menu end */}
-    </aside>
+                <Link
+                  className="flex h-full w-full items-center justify-center hover:scale-105"
+                  href={menu.href}
+                  onClick={CloseOffCanvasHandler}
+                >
+                  {menu.linkName}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        {/* map on menu end */}
+      </aside>
+    </>
   );
 }
