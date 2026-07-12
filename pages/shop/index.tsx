ac862d178fb99e10b55775c976ed80e77fe90456
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-07-12 13:29:45 +0330

    refactor(shop): use default db.json import for posters

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
-import { posters } from "@/public/db.json";
+import db from "@/public/db.json";
 
 export default function Shop() {
   return (
     <div className="wrapper mt-24 flex flex-col items-center sm:flex-row sm:justify-evenly">
-      {posters.map((poster) => (
+      {db.posters.map((poster) => (
         <Link key={poster.id} href={poster.href}>
           <Image
             width={1280}
