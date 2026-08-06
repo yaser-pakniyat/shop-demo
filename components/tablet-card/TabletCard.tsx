b80df3829bee704bffc96b59845260137a576199
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-08-06 15:33:42 +0330

    refactor(TabletCard): remove default button background

--- components/tablet-card/TabletCard.tsx
+++ components/tablet-card/TabletCard.tsx
@@ -24,7 +24,7 @@ export default function TabletCard({
       </Link>
 
       <Link
-        className="my-3 cursor-pointer rounded border bg-gray-300 px-3 py-1 hover:scale-90 hover:bg-green-500 hover:font-bold hover:text-white"
+        className="my-3 cursor-pointer rounded border px-3 py-1 hover:scale-90 hover:bg-green-500 hover:font-bold hover:text-white"
         href={`/shop/tabletstore/${productName}`}
       >
         more info
