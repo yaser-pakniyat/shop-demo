0b87492ebec5a0fd9071e315a0f3c8b438fc7bfa
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-08-13 00:15:38 +0330

    chore(StaffCard): add Staff interface for staff data

--- components/staff-card/StaffCard.tsx
+++ components/staff-card/StaffCard.tsx
@@ -1,10 +1,11 @@
 import db from "@/public/db.json";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import Staff from "../../types/staffTypes";
 
 export default function StaffCard() {
   return (
     <div className="grid grid-cols-12 gap-4">
-      {db.staff.map((staff) => (
+      {db.staff.map((staff: Staff) => (
         <div
           className="col-span-12 w-full rounded-2xl border-4 border-double border-lime-500 shadow-lime-700 hover:shadow-lg md:col-span-6 lg:col-span-4"
           key={staff.id}
