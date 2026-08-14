d18eb3e0fb6f1ce5d9f053b05272d43882d00677
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-08-14 13:57:18 +0330

    feat(About): add staffCard component

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,7 +1,10 @@
+import StaffCard from "@/components/staff-card/StaffCard";
+
 export default function About() {
   return (
     <div className="pages-wrapper">
       <h6 className="title">About Page</h6>
+      <StaffCard />
     </div>
   );
 }
