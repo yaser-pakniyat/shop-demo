196cb727485f9677b1a8086f9b5b42e0cccd7221
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-08-22 17:32:52 +0330

    feat(Hero): add hero content and shop link

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Hero() {
   return (
     <div>
-
+      <div>
+        <h1>Choose your device</h1>
+        <p>You could find your device easily with the best price</p>
+        <Link href="/shop" draggable={false}>
+          Discover Shop
+        </Link>
+      </div>
     </div>
   );
 }
