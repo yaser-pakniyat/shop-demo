749568463b4ca8493035747c4ad64d4a60d58c65
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-08-26 13:42:15 +0330

    style(Hero): add theme-based gradient

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -6,7 +6,9 @@ import styles from "./Hero.module.css";
 export default function Hero() {
   const theme = useSelector((store: RootState) => store.global.theme);
   return (
-    <div className={styles.heroWrapper}>
+    <div
+      className={`${styles.heroWrapper} ${theme === "dark" ? "from-slate-950 to-stone-900" : "from-teal-500 to-gray-50"}`}
+    >
       <div className={styles.contentWrapper}>
         <h1 className={styles.heading}>Choose your device</h1>
         <p className={styles.paragraph}>
