bb9f16cd0900915a7c065f6628f9dd39b75eb16b
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-09-03 23:00:11 +0330

    style(Hero): apply Baumans font to heading

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -2,6 +2,12 @@ import Link from "next/link";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import styles from "./Hero.module.css";
+import { Baumans } from "next/font/google";
+
+const baumans = Baumans({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function Hero() {
   const theme = useSelector((store: RootState) => store.global.theme);
@@ -10,7 +16,9 @@ export default function Hero() {
       className={`${styles.heroWrapper} ${theme === "dark" ? "from-slate-950 to-stone-900" : "from-teal-500 to-gray-50"}`}
     >
       <div className={styles.contentWrapper}>
-        <h1 className={styles.heading}>Choose your device</h1>
+        <h1 className={`${baumans.className} ${styles.heading}`}>
+          Choose your device
+        </h1>
         <p className={styles.paragraph}>
           You could find your device easily with the best price
         </p>
