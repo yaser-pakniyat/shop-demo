bc7eebaf49dd5169aeeb596afaef755ac19b51c6
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-09-21 00:13:53 +0330

    refactor(StaffCard): move styles to CSS module

--- components/staff-card/StaffCard.tsx
+++ components/staff-card/StaffCard.tsx
@@ -1,19 +1,17 @@
 import db from "@/public/db.json";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import Staff from "../../types/staffTypes";
+import styles from "./StaffCard.module.css";
 
 export default function StaffCard() {
   return (
-    <div className="grid grid-cols-12 gap-4">
+    <div className={styles.wrapper}>
       {db.staff.map((staff: Staff) => (
-        <div
-          className="col-span-12 w-full rounded-2xl border-4 border-double border-lime-500 shadow-lime-700 hover:shadow-lg md:col-span-6 lg:col-span-4"
-          key={staff.id}
-        >
-          <div className="flex w-full flex-col items-center-safe gap-y-3 p-1">
+        <div className={styles.cardWrapper} key={staff.id}>
+          <div className={styles.content}>
             {/* pic + name */}
             <Icon height="120" width="120" icon={staff.pic} />
-            <h4 className="self-center text-xl sm:text-4xl">{staff.name}</h4>
+            <h4 className={styles.name}>{staff.name}</h4>
 
             {/* title icon + title */}
             <div className="flex items-center">
