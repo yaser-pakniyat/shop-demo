5d337b3cb005e092a9def8f7e2996f53c91eaf05
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-09-22 16:31:18 +0330

    style(Technologies): update responsive heading font sizes

--- pages/technologies/index.tsx
+++ pages/technologies/index.tsx
@@ -4,7 +4,7 @@ import TypeWriter from "@/components/type-writer/Typewriter";
 export default function Technologies() {
   return (
     <div className="pages-wrapper">
-      <div className="mb-16 flex justify-center sm:text-3xl sm:font-bold">
+      <div className="text-md mb-16 flex justify-center sm:text-lg md:text-2xl lg:text-4xl sm:font-bold">
         <h1 className="mr-2 flex items-center">
           This website has been developed with
         </h1>
