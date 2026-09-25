2e6397f70e7e9ecfbd6d015013b3733943e252a6
yaser pakniyat <yaser.pakniyat.dev@gmail.com>
2026-09-25 18:12:14 +0330

    refactor(footer): move styles to CSS module

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,24 +2,25 @@ import { RootState } from "@/redux/store";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { useSelector } from "react-redux";
 import db from "@/public/db.json";
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   const theme = useSelector((store: RootState) => store.global.theme);
   return (
-    <footer className="flex w-full flex-col gap-y-4 pt-8 transition-colors duration-1000 ease-linear">
+    <footer className={styles.footer}>
       {/* address & phone section + social section start */}
-      <div className="w-full px-8">
+      <div className={styles.footerContent}>
         {/* address and phone section start */}
-        <div className="my-2 rounded-2xl border border-y-8 border-gray-400 p-4">
-          <address className="flex flex-col justify-between gap-y-8 px-3 md:flex-row">
+        <div className={styles.contactSection}>
+          <address className={styles.contactInfo}>
             <div>
-              <p className="font-bold not-italic">Address:</p>
+              <p className={styles.contactTitle}>Address:</p>
               <p>No.123</p>
               <p>example Street</p>
               <p>city, country</p>
             </div>
             <div>
-              <p className="font-bold not-italic">Inquiries:</p>
+              <p className={styles.contactTitle}>Inquiries:</p>
               <p>
                 Email:
                 <a aria-label="Email" href="mailto:">
@@ -39,7 +40,7 @@ export default function Footer() {
         {/* address and phone section end */}
 
         {/* social media section start*/}
-        <div className="flex justify-center space-x-4 rounded-2xl bg-gray-400 py-2">
+        <div className={styles.socialMedia}>
           {db.social.map((social) => (
             <div key={social.id} className="hover:scale-110">
               <a aria-label={social.name} href={social.address}>
@@ -54,7 +55,7 @@ export default function Footer() {
 
       {/* CopyRight section start */}
       <div
-        className={`transition-colors duration-1000 ease-linear ${
+        className={`${styles.copyright} ${
           theme === "dark"
             ? "bg-gray-950 text-gray-300"
             : "bg-gray-400 text-gray-950"
